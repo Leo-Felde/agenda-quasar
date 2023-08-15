c7f66f350927e2c0a776e0527b933d7acde93f6f
Leonardo H. Felde <leonardohfelde@gmail.com>
2023-08-15 09:42:20 -0300

    chore: removido TS e adicionado Eslint
    sim, eu odeio typescript.

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      babelrc: false,
+      configFile: false
+    },
+  },
+  extends: [
+    'plugin:nuxt/recommended',
+    'eslint:recommended',
+    'plugin:vue/recommended'
+  ],
+  plugins: [
+    'vue'
+  ],
+  rules: {
+    'indent': [
+      'error',
+      2
+    ],
+    'vue/multi-word-component-names': 'off',
+    'no-undef': 'off',
+    'linebreak-style': 0,
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  }
+}
---
+++ nuxt.config.js
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false,
+  // devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/eslint-module'
+  ],
+  eslint: {
+    fix: true
+  },
+})
