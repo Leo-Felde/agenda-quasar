34808dd9106bfeeae8ec8c325337d4bc1ffcc30f
Leonardo H. Felde <leonardohfelde@gmail.com>
2023-08-16 17:00:53 -0300

    feat: componente de ações de tabela

---
+++ components/tableOptionsBtn.vue
@@ -0,0 +1,66 @@
+<template>
+  <q-btn
+    icon="more_vert"
+    flat
+    round
+    @click.stop.prevent=""
+  >
+    <q-menu>
+      <q-list
+        v-if="!$slots.default"
+        style="min-width: 100px"
+      >
+        <q-item-label header>
+          Ações
+        </q-item-label>
+        <q-item
+          v-close-popup
+          clickable
+          class="row"
+          @click="$emit('editar', row)"
+        >
+          <q-item-section>
+            Editar
+          </q-item-section>
+          <q-item-section avatar>
+            <q-icon name="edit" />
+          </q-item-section>
+        </q-item>
+        <q-item
+          v-close-popup
+          clickable
+          @click="$emit('excluir', row)"
+        >
+          <q-item-section>
+            excluir
+          </q-item-section>
+          <q-item-section avatar>
+            <q-icon name="delete" />
+          </q-item-section>
+        </q-item>
+      </q-list>
+      <slot />
+    </q-menu>
+  </q-btn>
+</template>
+
+<script>
+export default {
+
+  props: {
+    row: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+
+  emits: ['editar'],
+  setup () {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
