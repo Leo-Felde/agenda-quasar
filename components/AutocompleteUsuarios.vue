b3155e9b75a765d87f358571ad6773f09aa94bf4
Leonardo H. Felde <leonardohfelde@gmail.com>
2023-08-17 08:29:54 -0300

    feat: autocomplete de usuários

---
+++ components/AutocompleteUsuarios.vue
@@ -0,0 +1,83 @@
+<template>
+  <q-select
+    v-model="usuarioSelecionado"
+    label="usuario"
+    option-label="nome"
+    use-input
+    filled
+    :options="usuarios"
+    :error="false"
+    @input-value="buscarUsuarios"
+    @update:modelValue="selecionarUsuario"
+  >
+    <template #no-option>
+      <q-item>
+        <q-item-section class="text-grey">
+          Nenhum usuario encontrado
+        </q-item-section>
+      </q-item>
+    </template>
+  </q-select>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+import UsuariosAPI from '~/api/usuarios'
+export default {
+  props: {
+    modelValue: { type: Object, default: () => {}}
+  },
+
+  emits: ['update:modelValue'],
+  setup (props, { emit }) {
+    const usuarios = ref([])
+    const usuarioSelecionado = ref(null)
+
+    const buscarUsuarios = async (event) => {
+      try {
+        const params = { termo: event }
+        const resp = await UsuariosAPI.pesquisar(params)
+        usuarios.value = resp.data
+      } catch (error) {
+        console.error(error)
+        $q.notify({
+          message: 'Não foi possivel pesquisar usuários',
+          position: 'top-right',
+          color: 'red'
+        })
+      }
+    }
+    const selecionarUsuario = () => {
+      emit('update:modelValue', usuarioSelecionado.value)
+    }
+
+    watch(() => props.modelValue, () => {
+      if (props.modelValue?.id) {
+        if (!usuarios.value.some(u => u.id === props.modelValue.id)) {
+          usuarios.value.push(props.modelValue)
+          setTimeout(() => {
+            usuarioSelecionado.value = props.modelValue
+          }, 100)
+        }
+      } else {
+        buscarUsuarios('')
+      }
+    })
+    
+    
+    onMounted(() => {
+      if (props.modelValue) {
+        usuarios.value.push(props.modelValue)
+      }
+    })
+
+    return {
+      usuarios,
+      buscarUsuarios,
+      selecionarUsuario,
+      usuarioSelecionado
+    }
+  }
+}
+</script>
