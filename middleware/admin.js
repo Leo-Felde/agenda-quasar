a4a5907dcc2c42ac8ce0b99874c8ee12ce32d077
Leonardo H. Felde <leonardohfelde@gmail.com>
2023-08-18 09:31:25 -0300

    fix: ajuste erro no middleware admin

--- middleware/admin.js
+++ middleware/admin.js
@@ -2,6 +2,12 @@ export default defineNuxtRouteMiddleware(() => {
   // middleware de páginas apenas para admins
 
   const user = useCurrentUser()
+  const storedUser = JSON.parse(localStorage.getItem('userData'))
+
+  if (!user?.value && storedUser) {
+    user.value = storedUser
+  }
+
   const isAdmin = user.value.tipos.includes('ROLE_ADMIN')
   if (!isAdmin) {
     return navigateTo('/')
